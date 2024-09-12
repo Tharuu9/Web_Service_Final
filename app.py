387c3ef74106035810cf081ddde221458336639c
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2024-09-12 20:06:59 +0530

    content updated

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-drop na 
+from flask import Flask # type: ignore
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '<h1>Hello, World!</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8001)
